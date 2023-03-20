4616af948e65579cdc8801732769d7302cf0ada6
Gunjan <gunjanv@yahoo.com>
2023-03-20 00:03:20 -0500

    Manifest changes

--- src/components/common/app-head.tsx
+++ src/components/common/app-head.tsx
@@ -4,10 +4,10 @@ export function AppHead(): JSX.Element {
   return (
     <Head>
       <title>Buzzwin</title>
-      <meta name='og:title' content='Twitter' />
+      <meta name='og:title' content='Buzzwin' />
       <link rel='icon' href='/favicon.ico' />
       <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
-      <meta name='buzzwin:site' content='@ccrsxx' />
+      <meta name='buzzwin:site' content='@buzzwinsite' />
       <meta name='buzzwin:card' content='summary_large_image' />
     </Head>
   );
